f62fb375be507b9c0eae6e1b7a79e8a1b2d5f08c
daecide <daecide@tokopedia.com>
2020-11-03 15:43:18 +0700

    add code

---
+++ errors.go
@@ -0,0 +1,68 @@
+package errors
+
+import (
+	def "errors"
+	"fmt"
+	"strings"
+)
+
+// New is just a wrapper for default package errors.New
+func New(text string) error {
+	return def.New(text)
+}
+
+// As is just a wrapper for default package errors.As
+func As(err error, target interface{}) bool {
+	return def.As(err, target)
+}
+
+// Is is just a wrapper for default package errors.Is
+func Is(err error, target error) bool {
+	return def.Is(err, target)
+}
+
+// Unwrap is just a wrapper for default package errors.Unwrap
+func Unwrap(err error) error {
+	return def.Unwrap(err)
+}
+
+// RootCause will get the first error
+func RootCause(err error) error {
+	root := err
+
+	for {
+		temp := def.Unwrap(err)
+
+		if temp == nil {
+			break
+		}
+
+		root = temp
+	}
+
+	return root
+}
+
+// Traces will print the wrapped error stack
+func Traces(err error) error {
+	traces := []string{err.Error()}
+
+	i := 0
+	for {
+		i = i + 1
+		temp := def.Unwrap(err)
+
+		if temp == nil {
+			break
+		}
+
+		pre := ""
+		for j := 0; j < i; j++ {
+			pre = fmt.Sprintf("%s ", pre)
+		}
+
+		traces = append(traces, fmt.Sprintf("%s-%s", pre, temp.Error()))
+	}
+
+	return def.New(strings.Join(traces, "\n"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jjajan-id/errors
+
+go 1.13
